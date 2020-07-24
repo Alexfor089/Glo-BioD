e7f212cadb1795b03073b7b87fafd33fa1076540
Hristofor Alekseev <hristofor2003@mail.ru>
2020-07-25 00:47:31 +0500

    Все работает,скроллов нет

--- src/modules/popupToogleCallForm.js
+++ src/modules/popupToogleCallForm.js
@@ -1,37 +1,32 @@
 const popupToogleCallForm = () => {
-
-  let popupCall = document.querySelector('.popup-call'),
-    callBtns = document.querySelectorAll('.call-btn')
+  let popupCall = document.querySelector(".popup-call"),
+    callBtns = document.querySelectorAll(".call-btn");
   callBtns.forEach((item) => {
     item.addEventListener("click", () => {
-
       popupCall.style.cssText = `
-       
+      opacity: 0;
        opacity: 0;
        transition: all .4s;
        display: block;
        `;
-      document.body.style.cssText = `overflow: hidden;`
+      document.body.style.cssText = `overflow: hidden;`;
       setTimeout(() => {
         popupCall.style.opacity = "1";
       }, 50);
-
     });
   });
 
-
   popupCall.addEventListener("click", (e) => {
-    // 
+    //
 
     e.preventDefault();
-    document.body.style.cssText = `overflow: scroll;`
+    document.body.style.cssText = `overflow: scroll;`;
     let target = e.target;
     if (target.classList.contains("popup-close")) {
       popupCall.style.cssText = `display: none;`;
       setTimeout(() => {
         popupCall.style.opacity = "1";
       }, 50);
-
     } else {
       target = target.closest(".popup-content");
       if (!target) {
@@ -39,8 +34,6 @@ const popupToogleCallForm = () => {
       }
     }
   });
-
 };
-
 
 export default popupToogleCallForm;
